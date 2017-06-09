607ae93c46f09802f4a6b28dd6464a44378a54e8
blueseasky <githubwork@126.com>
2017-06-09 15:06:17 +0800

    restful api test

---
+++ src/main/java/com/blueseasky/springboot/domain/User.java
@@ -0,0 +1,7 @@
+package com.blueseasky.springboot.domain;
+
+/**
+ * Created by renlei on 2017/6/9.
+ */
+public class User {
+}
---
+++ src/main/java/com/blueseasky/springboot/facade/UserController.java
@@ -0,0 +1,7 @@
+package com.blueseasky.springboot.facade;
+
+/**
+ * Created by renlei on 2017/6/9.
+ */
+public class UserController {
+}
